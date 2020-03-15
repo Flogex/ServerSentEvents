9c9161fbe40037650823c4b885cacf1146bdcbef
Florian Gerlinghoff <gerlinghoff@live.de>
2020-03-15 12:34:04 +0100

    Split HttpClientTests into two files

---
+++ test/ServerSentEvents.Test.Unit/HttpClientTests_CloseConnection.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using ServerSentEvents.Test.Unit.Fakes;
+using Xunit;
+
+namespace ServerSentEvents.Test.Unit
+{
+    public class HttpClientTests_CloseConnection
+    {
+        [Fact]
+        public async Task WhenConnectionIsClosed_HttpContextShouldBeAborted()
+        {
+            var context = FakeHttpContext.NewHttpContext();
+            var sut = await HttpClient.NewClient(context);
+
+            await sut.CloseConnection();
+
+            context.RequestAborted.IsCancellationRequested.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task WhenConnectionIsClosed_HttpResponseShouldBeCompleted()
+        {
+            var context = FakeHttpContext.NewHttpContext();
+            var sut = await HttpClient.NewClient(context);
+            var responseCompleted = false;
+
+            context.Response.OnCompleted(() =>
+            {
+                responseCompleted = true;
+                return Task.CompletedTask;
+            });
+
+            await sut.CloseConnection();
+
+            responseCompleted.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task WhenConnectionIsClosedByServer_ConnectionClosedEventShouldBeRaised()
+        {
+            var sut = await HttpClient.NewClient(FakeHttpContext.NewHttpContext());
+            using var eventMonitor = sut.Monitor();
+
+            await sut.CloseConnection();
+
+            eventMonitor.Should().Raise(nameof(sut.ConnectionClosed)).WithSender(sut);
+        }
+
+        [Fact]
+        public async Task WhenConnectionIsClosedByClient_ConnectionClosedEventShouldBeRaised()
+        {
+            var context = FakeHttpContext.NewHttpContext();
+            var sut = await HttpClient.NewClient(context);
+            using var eventMonitor = sut.Monitor();
+
+            context.Abort();
+
+            eventMonitor.Should().Raise(nameof(sut.ConnectionClosed)).WithSender(sut);
+        }
+    }
+}
---
+++ test/ServerSentEvents.Test.Unit/HttpClientTests_NewClient.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using ServerSentEvents.Test.Unit.Fakes;
+using Xunit;
+
+namespace ServerSentEvents.Test.Unit
+{
+    public class HttpClientTests_NewClient
+    {
+        [Fact]
+        public async Task IfHttpResponseHasAlreadyStarted_ShouldThrowInvalidOperationException()
+        {
+            var context = FakeHttpContext.NewHttpContext();
+            await context.Response.StartAsync();
+
+            Func<Task> createClientAction = async () =>
+                await HttpClient.NewClient(context);
+
+            createClientAction.Should().Throw<InvalidOperationException>()
+                                       .WithMessage("Response has already started.");
+        }
+
+        private async Task<HttpResponse> GetHttpResponseOfNewClient()
+        {
+            var context = FakeHttpContext.NewHttpContext();
+            var sut = await HttpClient.NewClient(context);
+            return sut.HttpContext.Response;
+        }
+
+        [Fact]
+        public async Task StatusCodeShouldBeOK()
+        {
+            var response = await GetHttpResponseOfNewClient();
+            response.StatusCode.Should().Be(200);
+        }
+
+        [Fact]
+        public async Task ContentTypeShouldBeEventStream()
+        {
+            var response = await GetHttpResponseOfNewClient();
+            response.ContentType.Should().Be("text/event-stream");
+        }
+
+        [Fact]
+        public async Task CacheControlHeaderShouldBeNoCache()
+        {
+            var response = await GetHttpResponseOfNewClient();
+            var cachingHeaders = response.Headers["Cache-Control"];
+            cachingHeaders.Should().HaveCount(1).And.Contain("no-cache");
+        }
+
+        [Fact]
+        public async Task IfRequestDoesNotContainLastEventId_LastEventIdPropertyShouldBeNull()
+        {
+            var context = FakeHttpContext.NewHttpContext();
+            var sut = await HttpClient.NewClient(context);
+            sut.LastEventId.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task IfRequestContainsLastEventId_LastEventIdPropertyShouldBeSetToThisValue()
+        {
+            var context = FakeHttpContext.NewHttpContext();
+            context.Request.Headers.Add("Last-Event-Id", "someId");
+
+            var sut = await HttpClient.NewClient(context);
+
+            sut.LastEventId.Should().Be("someId");
+        }
+    }
+}
