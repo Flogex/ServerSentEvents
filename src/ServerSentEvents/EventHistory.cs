6dca171aba9c89eae0630c822f580d17bad541e5
Florian Gerlinghoff <gerlinghoff@live.de>
2020-03-17 17:58:52 +0100

    Added EventHistory
    The class is going to be used for the Resend feature. It stores all
    (Data) Events that were broadcastet. All events that came after one with
    a specified id can be retrieved.

---
+++ src/ServerSentEvents/EventHistory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ServerSentEvents.Events;
+
+namespace ServerSentEvents
+{
+    internal class EventHistory
+    {
+        // Most recent events at start of list
+        private readonly LinkedList<Event> _events = new LinkedList<Event>();
+
+        public void Add(Event @event) => _events.AddFirst(@event);
+
+        public IEnumerable<Event> GetSubsequentEvents(string eventId)
+        {
+            var eventIndex = _events.Find(e => e.Id == eventId);
+            // Takes zero elements if eventIndex == -1
+            return _events.Take(eventIndex);
+        }
+    }
+
+    internal static class LinkedListExtensions
+    {
+        public static int Find<T>(
+            this LinkedList<T> list,
+            Predicate<T> predicate)
+        {
+            var index = 0;
+            foreach (var item in list)
+            {
+                if (predicate(item))
+                    return index;
+
+                index++;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ test/ServerSentEvents.Test.Unit/EventHistoryTests_GetSubsequentEvents.cs
@@ -0,0 +1,111 @@
+﻿using FluentAssertions;
+using ServerSentEvents.Events;
+using Xunit;
+
+namespace ServerSentEvents.Test.Unit
+{
+    public class EventHistoryTests_GetSubsequentEvents
+    {
+        private readonly EventHistory _sut = new EventHistory();
+
+        private void AddEventsInOrder(params Event[] events)
+        {
+            foreach (var @event in events)
+                _sut.Add(@event);
+        }
+
+        [Fact]
+        public void WhenGettingEventsAfterMostRecent_ShouldReturnEmptyCollection()
+        {
+            _sut.Add(new Event("_", id: "42"));
+
+            var moreRecentEvents = _sut.GetSubsequentEvents("42");
+
+            moreRecentEvents.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void WhenGettingEventsAfterSecondMostRecent_ShouldReturnMostRecentEvent()
+        {
+            var secondMostRecent = new Event("_", id: "1");
+            var mostRecent = new Event("_", id: "2");
+
+            AddEventsInOrder(secondMostRecent, mostRecent);
+
+            var moreRecentEvents = _sut.GetSubsequentEvents(secondMostRecent.Id);
+
+            moreRecentEvents.Should().BeEquivalentTo(mostRecent);
+        }
+
+        [Fact]
+        public void WhenGettingEventsAfterOldest_ShouldReturnAllEventsButTheOldest()
+        {
+            var first = new Event("_", id: "1");
+            var second = new Event("_", id: "2");
+            var third = new Event("_", id: null);
+            var fourth = new Event("_", id: "3");
+
+            AddEventsInOrder(first, second, third, fourth);
+
+            var moreRecentEvents = _sut.GetSubsequentEvents(first.Id);
+
+            moreRecentEvents.Should().BeEquivalentTo(second, third,
+                fourth);
+        }
+
+        [Fact]
+        public void WhenIdIsNonExistent_ShouldReturnEmptyCollection()
+        {
+            AddEventsInOrder(
+                new Event("_", id: "1"),
+                new Event("_", id: "2"));
+
+            var moreRecentEvents = _sut.GetSubsequentEvents("nonExistentId");
+
+            moreRecentEvents.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void IfOnlyEventsWithoutIdStored_ShouldReturnEmptyCollection()
+        {
+            AddEventsInOrder(
+                new Event("_", id: null),
+                new Event("_", id: null));
+
+            var newerEvents = _sut.GetSubsequentEvents("someId");
+
+            newerEvents.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void ShouldNotReturnEventsEarlierThanEventWithGivenId()
+        {
+            var first = new Event("_", id: "40");
+            var second = new Event("_", id: null);
+            var third = new Event("_", id: "42");
+            var fourth = new Event("_", id: "43");
+
+            AddEventsInOrder(first, second, third, fourth);
+
+            var newerEvents = _sut.GetSubsequentEvents(third.Id);
+
+            newerEvents.Should().NotContain(new[] { first, second, third });
+        }
+
+        [Fact]
+        public void IfTwoEventsWithSameIdAreStored_LatestShouldBeUsed()
+        {
+            var first = new Event("_", id: "1");
+            var second = new Event("_", id: "2");
+            var third = new Event("_", id: "1");
+            var fourth = new Event("_", id: "3");
+
+            AddEventsInOrder(first, second, third, fourth);
+
+            var newerEvents = _sut.GetSubsequentEvents("1");
+
+            newerEvents.Should().NotContain(second)
+                       .And.Contain(fourth);
+        }
+    }
+}
